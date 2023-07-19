37cbdb86af72981e2c8377db96228c3adb878330
jang_yoonsu <shagrat8546@gmail.com>
2023-07-20 00:28:29 +0900

    Feat: 구글맵 조건칠하기

--- src/GoogleMap.tsx
+++ src/GoogleMap.tsx
@@ -12,22 +12,65 @@ const center = {
   lng: 126.9786567,
 };
 
+const options: { [key: string]: { point: number } } = {
+  Gangdong: { point: 0 },
+  Songpa: { point: 1100 },
+  Gangnam: { point: 2200 },
+  Seocho: { point: 0 },
+  Gwanak: { point: 5000 },
+  Dongjak: { point: 0 },
+  Yeongdeungpo: { point: 0 },
+  Geumcheon: { point: 2300 },
+  Guro: { point: 0 },
+  Gangseo: { point: 0 },
+  Yangcheon: { point: 3300 },
+  Mapo: { point: 0 },
+  Seodaemun: { point: 0 },
+  Eunpyeong: { point: 0 },
+  Nowon: { point: 0 },
+  Dobong: { point: 500 },
+  Gangbuk: { point: 1300 },
+  Seongbuk: { point: 0 },
+  Jungnang: { point: 0 },
+  Dongdaemun: { point: 0 },
+  Gwangjin: { point: 0 },
+  Seongdong: { point: 0 },
+  Yongsan: { point: 0 },
+  Jung: { point: 0 },
+  Jongno: { point: 4500 },
+};
+
 function Map() {
-  const [polygons, setPolygons] = useState<{ lat: number; lng: number }[][]>(
-    []
-  );
+  const [polygons, setPolygons] = useState<
+    {
+      name: string;
+      name_eng: string;
+      path: { lat: number; lng: number }[];
+      options: {};
+    }[]
+  >([]);
 
   useEffect(() => {
     const data = seoulData.features.map((feature) => {
-      return feature.geometry.coordinates[0].map(([lng, lat]) => ({
+      const name = feature.properties.name;
+      const name_eng = feature.properties.name_eng;
+      const path = feature.geometry.coordinates[0].map(([lng, lat]) => ({
         lat,
         lng,
       }));
+
+      console.log(feature.properties.name_eng);
+      return { name, name_eng, path, options: {} };
     });
 
     setPolygons(data);
+
+    return () => setPolygons([]);
   }, []);
 
+  if (polygons.length === 0) return <>Loading...</>;
+
+  console.log(polygons);
   return (
     <LoadScript googleMapsApiKey="AIzaSyB3ixB-V1mYdr7uNucQaUs_z3lOlVc4XzA">
       <GoogleMap
@@ -36,18 +79,23 @@ function Map() {
         zoom={11}
         options={{ draggable: false }}
       >
-        {polygons.map((path, index) => {
+        {polygons.map((polygon, index) => {
+          const name_eng = polygon.name_eng.slice(0, -3);
+          const point = options[name_eng].point;
+          let opacity =
+            point < 1000 ? 0.9 : point < 2000 ? 0.7 : point < 3000 ? 0.5 : 0.3;
           return (
             <Polygon
               key={index}
-              paths={path}
+              paths={polygon.path}
               options={{
-                fillColor: "#000",
-                fillOpacity: 0.8,
-                strokeColor: "#fff",
+                fillColor: "#222",
+                fillOpacity: opacity,
+                strokeColor: "#555",
                 strokeOpacity: 1,
-                strokeWeight: 2,
+                strokeWeight: 1,
               }}
+              onClick={() => alert("바보")}
             />
           );
         })}
